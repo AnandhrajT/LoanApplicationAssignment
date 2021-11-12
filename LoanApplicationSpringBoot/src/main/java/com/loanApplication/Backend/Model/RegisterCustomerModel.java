88f25fa0ccb1cee7969d83a05a42e12d99fc3a47
Anandharaj T <81517285+AnandhrajT@users.noreply.github.com>
2021-11-12 16:57:29 +0530

    Update RegisterCustomerModel.java

--- LoanApplicationSpringBoot/src/main/java/com/loanApplication/Backend/Model/RegisterCustomerModel.java
+++ LoanApplicationSpringBoot/src/main/java/com/loanApplication/Backend/Model/RegisterCustomerModel.java
@@ -1,60 +1,49 @@
-package com.loanApplication.Backend.Model;
+package com.example.demo.Model;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.Table;
 
 @Entity
-@Table
-public class RegisterCustomerModel {
-	
+public class UserModel {
+
 	@Id
-	@Column
-	private String customerId;
-	@Column
-	private String name;
-	@Column
 	private long accountNumber;
-	@Column
-	private String email;
-	@Column
-	private Long phone;
-	@Column
+	private String name;
+	private String emailId;
 	private String password;
-	public String getCustomerId() {
-		return customerId;
+	private String confirmPassword;
+	
+	public UserModel() {
 	}
-
-	public long getAccountNumber() {
-		return accountNumber;
-	}
-
-	public void setAccountNumber(long accountNumber) {
+	
+	public UserModel(String name, String emailId, long accountNumber, String password, String confirmPassword) {
+		super();
+		this.name = name;
+		this.emailId = emailId;
 		this.accountNumber = accountNumber;
+		this.password = password;
+		this.confirmPassword = confirmPassword;
+	}
+	
+	public String getEmailId() {
+		return emailId;
 	}
 
-	public void setCustomerId(String customerId) {
-		this.customerId = customerId;
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
 	}
+
 	public String getName() {
 		return name;
 	}
 	public void setName(String name) {
 		this.name = name;
 	}
-	
-	public String getEmail() {
-		return email;
+	public long getAccountNumber() {
+		return accountNumber;
 	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public Long getPhone() {
-		return phone;
-	}
-	public void setPhone(Long phone) {
-		this.phone = phone;
+	public void setAccountNumber(long accountNumber) {
+		this.accountNumber = accountNumber;
 	}
 	public String getPassword() {
 		return password;
@@ -62,4 +51,11 @@ public class RegisterCustomerModel {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	public String getConfirmPassword() {
+		return confirmPassword;
+	}
+	public void setConfirmPassword(String confirmPassword) {
+		this.confirmPassword = confirmPassword;
+	}
+	
 }
